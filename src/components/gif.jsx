f7b46c644daf462b364f679af443602f922f2a5f
Andreea Capalnas <andreea.capalnas@gmail.com>
2019-09-14 01:10:08 +0100

    added api to the search function along with onClick event for the gifs; pre-determined search in place to

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  
   render () {
     const src = `https://media3.giphy.com/media/${this.props.id}/200w.gif`;
     return (
-      <img src={src} alt="" className="gif"/>
+      <img src={src} alt="" className="gif" width="700px"
+        onClick={ () => this.props.changeGifFunction(this.props.id) }
+        // onClick={console.log}
+      />
     );
   }
 }
